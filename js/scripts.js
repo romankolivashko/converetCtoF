5dca26d34213847ea4d2af54d83e38006cf4800d
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-11 17:09:29 -0700

    Ready to push remote

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+// Business logic:
+// let number1;
+function convert(number1) {
+  const result = (number1 * 9/5) + 32;
+  return result;
+}
+
+// function convertCtoF(num1){
+// 	const result = (num1 * 9/5) + 32;
+//   return result;
+// }
+
+// User interface logic:
+
+$(document).ready(function() {
+  $("form#convert").submit(function(event) {
+   event.preventDefault();
+    const number1 = parseInt($("#convert1").val());
+    const result = convert(number1);
+    $("#output").text(result);
+  });
+});
+
+
+
+
+
+
+
+
+
+// function convertCtoF(num1){
+// 	const result = (num1 * 9/5) + 32;
+//   return result;
+// }
+
+// const num1 = parseInt(prompt("Enter C temperature:"));
+// const fResult = convertCtoF(num1);
+// alert(fResult);
+
+
+// function convertFtoC(num1){
+// 	const result = (num1 - 32) * 5/9;
+//   return result;
+// }
+
+// const num1 = parseInt(prompt("Enter F temperature:"));
+// const fResult = convertFtoC(num1);
+// alert(fResult);
